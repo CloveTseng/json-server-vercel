fe60e1ba539821271bf0b18807c7aeaac5ad0a79
DolceTseng <jessica.turn613@gmail.com>
2024-12-08 18:03:53 +0800

    add swagger

--- server.js
+++ server.js
@@ -1,19 +1,38 @@
-// See https://github.com/typicode/json-server#module
 const cors = require('cors');
-const jsonServer = require('json-server')
-const server = jsonServer.create()
+const jsonServer = require('json-server');
+const path = require('path');
 const auth = require("json-server-auth");
+
+const server = jsonServer.create();
 const db = require("./db.json");
 const router = jsonServer.router(db);
 const middlewares = jsonServer.defaults();
-server.use(cors())
-server.use(middlewares)
+
+server.use(cors());
+server.use(middlewares);
 server.db = router.db;
+
+// 使用 json-server-auth
 server.use(auth);
-server.use(router)
+
+// 提供 Swagger JSON 文件
+server.use('/swagger', (req, res) => {
+    res.sendFile(path.join(__dirname, 'swagger.json'));
+});
+
+// 提供 Swagger UI 静态文件
+const swaggerUiPath = require('swagger-ui-dist').absolutePath();
+server.use('/docs', jsonServer.defaults({ static: swaggerUiPath }));
+
+// 加载路由
+server.use(router);
+
+// 启动服务器
 server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+    console.log('JSON Server is running');
+    console.log('Swagger JSON available at: http://localhost:3000/swagger');
+    console.log('Swagger UI available at: http://localhost:3000/docs');
+});
 
 // Export the Server API
-module.exports = server
+module.exports = server;
